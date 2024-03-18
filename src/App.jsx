2f46b92bc228824602a34ffc99eae3f082757fa3
Sanket <sanketj@trentiums.com>
2024-03-18 15:59:09 +0530

    api not working

--- src/App.jsx
+++ src/App.jsx
@@ -5,83 +5,83 @@ import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
 
-  const [amount,setAmount] = useState(0);
-  const [convertedAmount,setConvertedAmount] = useState(0);
-  const [from,setFrom] = useState("usd");
-  const [to,setTo] = useState("inr");
+    const [amount, setAmount] = useState(0);
+    const [convertedAmount, setConvertedAmount] = useState(0);
+    const [from, setFrom] = useState("usd");
+    const [to, setTo] = useState("inr");
 
-  const currencyInfo = useCurrencyInfo(from);
-  const options = Object.keys(currencyInfo);
+    const currencyInfo = useCurrencyInfo(from);
+    const options = Object.keys(currencyInfo);
 
-  const convert = ()=>{
-    setConvertedAmount(amount*currencyInfo[to])
-  }
+    const convert = () => {
+        setConvertedAmount(amount * currencyInfo[to])
+    }
 
-  const swap = ()=>{
-    setFrom(to);
-    setTo(from);
-    setAmount(convertedAmount);
-    setConvertedAmount(amount)
-  }
+    const swap = () => {
+        setFrom(to);
+        setTo(from);
+        setAmount(convertedAmount);
+        setConvertedAmount(amount)
+    }
 
-//   useEffect(()=>{
-//     convert();
-//   },[from,to,amount])
+    useEffect(() => {
+        convert();
+    }, [from, to, amount])
 
 
-  return (
-    <div
-        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-        style={{
-            backgroundImage: `url('https://img.freepik.com/premium-vector/global-currency-background_115579-405.jpg')`,
-        }}
-    >
-        <div className="w-full">
-            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        convert();
-                    }}
-                >
-                    <div className="w-full mb-1">
-                        <InputBox
-                            label="From"
-                            amount={amount}
-                            selectCurrency={from}
-                            onAmountChange={(amount)=>setAmount(amount)}
-                            currencyOptions={options}
-                            onCurrencyChange={(cur)=>setFrom(cur)}
-                        />
-                    </div>
-                    <div className="relative w-full h-0.5">
-                        <button
-                            type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                            onClick={swap}
-                        >
-                            swap
+    return (
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://img.freepik.com/premium-vector/global-currency-background_115579-405.jpg')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert();
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                selectCurrency={from}
+                                onAmountChange={(amount) => setAmount(amount)}
+                                currencyOptions={options}
+                                onCurrencyChange={(cur) => setFrom(cur)}
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={convertedAmount}
+                                selectCurrency={to}
+                                onAmountChange={(amount) => setAmount(amount)}
+                                currencyOptions={options}
+                                onCurrencyChange={(cur) => setTo(cur)}
+                                disable
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
-                    </div>
-                    <div className="w-full mt-1 mb-4">
-                        <InputBox
-                            label="To"
-                            amount={convertedAmount}
-                            selectCurrency={to}
-                            onAmountChange={(amount)=>setAmount(amount)}
-                            currencyOptions={options}
-                            onCurrencyChange={(cur)=>setTo(cur)}
-                            disable
-                        />
-                    </div>
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-                        Convert {from.toUpperCase()} to {to.toUpperCase()}
-                    </button>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 
 }
 
